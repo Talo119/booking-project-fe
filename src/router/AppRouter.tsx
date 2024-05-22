cd81fc62fc40028d0fc3dc26ebe0890f61f10278
Carlos Motino <carlos.motino@alcon.com>
2024-05-22 10:48:45 -0600

    fixing protected routes.

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,33 +1,26 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import { AuthRoutes } from "../auth/routes/AuthRoutes";
-import { BookingAdminRoutes } from "../bookingAdmin/routes/BookingAdminRoutes";
-import { useSelector } from "react-redux";
-import { selectLoginData, selectLoginState } from "../store/auth/loginSlice";
-import { LoginPage } from "../auth/pages";
+import { Navigate, Route, Routes } from "react-router-dom"
+import { AuthRoutes } from "../auth/routes/AuthRoutes"
+import { BookingAdminRoutes } from "../bookingAdmin/routes/BookingAdminRoutes"
+import { useSelector } from 'react-redux';
+import { selectLoginState, selectLoginData } from '../store/auth/loginSlice';
 
 export const AppRouter = () => {
-  const userState = useSelector(selectLoginState);
-  const authenticatedUser = useSelector(selectLoginData);
-  return (
-    <Routes>
-      {/* Login y Registro */}
-      <Route />
+    const userState = useSelector(selectLoginState);
+    const authenticatedUser = useSelector(selectLoginData)
+    return (
+        <Routes>
+            {/* Login y Registro */}
+            <Route path="/auth/*" element={ <AuthRoutes/> } />
 
-      {/* Admin */}
-      <Route path="/auth/*" element={<AuthRoutes />} />
-
-      {/* Client */}
-      {userState === 1 &&
-      authenticatedUser &&
-      authenticatedUser.roles.some((rol) => rol === "ADMIN_ROLE") ? (
-        <Route path="/admin/*" element={<BookingAdminRoutes />} />
-      ) : userState === 1 &&
-        authenticatedUser &&
-        authenticatedUser.roles.some((rol) => rol === "BUYER_ROLE") ? (
-        <Route path="/client/*" />
-      ) : (
-        <Route path="/auth/login" element={ <LoginPage/>} />
-      )}
-    </Routes>
-  );
-};
+            {/* Admin */}
+            {
+                (userState === 1 && authenticatedUser.roles.some((rol) => rol === "ADMIN_ROLE"))
+                ? <Route path="/admin/*" element={ <BookingAdminRoutes /> } />
+                : <Route path="/auth/*" element={ <AuthRoutes/> } />
+            }
+            <Route path="/*" element={<Navigate to='/auth/login'/>} />
+            {/* Client */}
+            // TODO: pending create routes for client pages.
+        </Routes>
+    )
+}
